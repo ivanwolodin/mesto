9575d4552809853df9a3eb7afcca24ce0ce800b6
Ivan <ivanwolodin@gmail.com>
2021-11-27 17:05:39 +0500

    add: autogeneratings cards and delete icon

--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,56 @@ const popupPosition = popup.querySelector(".popup__subtitle_type_profession");
 let profileName = document.querySelector(".profile__name");
 let profilePosition = document.querySelector(".profile__position");
 
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const elementsTemplate = document.querySelector('.elements-cards').content;
+const elements = document.querySelector('.elements');
+
+initialCards.forEach((item) => {
+  const element =  elementsTemplate.cloneNode(true);
+  element.querySelector('.element__image').src = item.link;
+  element.querySelector('.element__name').textContent = item.name;
+  elements.append(element);
+
+}); 
+
+// const directorTemplate = document.querySelector('.director-template').content;
+// directors.forEach(function (element) {
+//   const directorElement = directorTemplate.cloneNode(true);
+
+//   directorElement.querySelector('.directors__name').textContent = element.name;
+//   directorElement.querySelector('.directors__description').textContent = element.career;
+//   directorElement.querySelector('.directors__films').href = element.films;
+
+//   directorsList.append(directorElement)
+// })
+
+
 function openPopup() {
   popupName.value = profileName.textContent;
   popupPosition.value = profilePosition.textContent;
